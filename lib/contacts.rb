9f770b3f97eb3bb68dc0671b0e9513cffe6ba42c
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-12 23:12:45 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |atrribute, data|
+    contact_details_hash.each do |attribute, data|
       binding.pry
       if value == :favorite_ice_cream_flavors
       value.each do |flavor|
